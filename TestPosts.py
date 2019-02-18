1540cb58f4adc239b69a5bdaf40d2ac671c7181f
Angela Korra'ti <annathepiper@fastmail.com>
2019-02-18 13:03:20 -0800

    Initial draft of TestPosts class

---
+++ TestPosts.py
@@ -0,0 +1,29 @@
+from BaseTest import BaseTest
+
+# TestPosts
+# Written by Angela Korra'ti
+# Last updated 2/18/2019
+#
+# This test class is for verifying the posts-related endpoints for the Wordpress test site.
+
+
+class TestPosts(BaseTest):
+
+    def test_get_posts_returns_posts(self):
+        """
+        Verify the GetPosts endpoint returns a JSON array of length > 0
+        """
+        req = self.wp_tc.get_posts()
+        assert req is not None, "GetPosts endpoint returned a null response."
+        assert len(req) > 0, "GetPosts endpoint not returning at least one object in JSON output."
+
+    def test_get_post_by_id(self):
+        """
+        Verify getting a post by specific ID off the GetPosts endpoint
+        :return:
+        """
+        req = self.wp_tc.get_post_by_id(self.wp_lib.getPostId)
+        assert req is not None, "GetPosts endpoint returned a null object. Post may not exist."
+        assert req['id'] == self.wp_lib.getPostId, "GetPosts endpoint didn't return correct ID number."
+        assert req['title']['rendered'] == self.wp_lib.getPostTitle,\
+            "Retrieved post from GetPosts endpoint does not have expected title."
