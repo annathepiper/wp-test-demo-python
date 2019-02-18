c45770eb4036a28ac0e651fb3421d89d819b8231
Angela Korra'ti <annathepiper@fastmail.com>
2019-02-18 13:03:46 -0800

    Initial draft of WPTestLib

---
+++ WPTestLib.py
@@ -0,0 +1,29 @@
+# WPTestLib
+# Written by Angela Korra'ti
+# Last updated 2/18/19
+#
+# This is a helper class providing access to assorted strings for IDs and such for configuring and running tests.
+
+class WPTestLib:
+    protocol = "http"
+    host = "wordpress.local"
+    getPostId = 25428
+    getPostTitle = "Hello, readers!"
+    getCategoryId = 1
+    getCategoryName = "Uncategorized"
+    getTagId = 7
+    getTagName = "administrivia"
+    getPageId = 1087
+    getPageTitle = "Faerie Blood"
+    getCommentId = 2
+    getCommentContent = "This is a comment for testing purposes, used by my test automation suite."
+    getTaxonomyTag = "category"
+    getTaxonomyName = "Categories"
+    getMediaId = 25550
+    getMediaTitle = "cropped-faerie-blood-epub-cover1.jpg"
+    getUserId = 1
+    getUserName = "annathepiper"
+    getPostTypeTag = "post"
+    getPostTypeName = "Posts"
+    getPostStatusTag = "publish"
+    getPostStatusName = "Published"
