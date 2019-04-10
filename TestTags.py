cab3d9fca205efd24d0acf8ce638260eb2280f7f
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-10 09:58:49 -0700

    New negative test cases
    Adding new negative test cases as ported over from the Java suite.

--- TestTags.py
+++ TestTags.py
@@ -2,7 +2,7 @@ from BaseTest import BaseTest
 
 # TestTags
 # Written by Angela Korra'ti
-# Last updated 2/18/2019
+# Last updated 4/10/2019
 #
 # This test class is for verifying the tags-related endpoints for the WordPress test site.
 
@@ -26,3 +26,33 @@ class TestTags(BaseTest):
         assert req['id'] == self.wp_lib.getTagId, "GetTagById endpoint didn't return correct ID number."
         assert req['name'] == self.wp_lib.getTagName,\
             "Retrieved tag from GetTagById endpoint does not have expected name."
+
+    def test_get_tag_id_that_does_not_exist(self):
+        """
+        Verify that the Get Tag by Id endpoint exhibits expected error behavior for a tag ID that doesn't exist
+        """
+        req = self.wp_tc.get_tag_by_id(self.wp_lib.getNonExistentId)
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getTermNonExistentCode,
+                                                        self.wp_lib.getTermNonExistentMessage)
+
+    def test_get_tag_id_bad_id(self):
+        """
+        Verify that the Get Tag by Id endpoint throws expected error for an invalid tag ID
+        """
+        req = self.wp_tc.get_tag_by_id(self.wp_lib.getInvalidId)
+        self.wp_lib.verify_response_item_is_invalid(req, self.wp_lib.getInvalidCode, self.wp_lib.getInvalidMessage)
+
+    def test_get_tag_id_max_size(self):
+        """
+        Verify that the Get Tag by Id endpoint throws expected error for a tag ID using sys.maxsize
+        """
+        req = self.wp_tc.get_tag_by_id(str(self.maxsize))
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getTermNonExistentCode,
+                                                        self.wp_lib.getTermNonExistentMessage)
+
+    def test_get_tag_id_min_size(self):
+        """
+        Verify that the Get Tag by Id endpoint throws expected error for a tag ID using -sys.maxsize
+        """
+        req = self.wp_tc.get_tag_by_id(str(self.minsize))
+        self.wp_lib.verify_response_item_is_invalid(req, self.wp_lib.getInvalidCode, self.wp_lib.getInvalidMessage)
