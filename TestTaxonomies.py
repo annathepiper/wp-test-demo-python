529a11ff788a067936dd582630d4ec5878b74ae6
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-10 10:09:42 -0700

    New negative test cases
    Adding new negative test cases as ported over from the Java suite.

--- TestTaxonomies.py
+++ TestTaxonomies.py
@@ -2,7 +2,7 @@ from BaseTest import BaseTest
 
 # TestTaxonomies
 # Written by Angela Korra'ti
-# Last updated 2/18/2019
+# Last updated 4/10/2019
 #
 # This test class is for verifying the taxonomies-related endpoints for the WordPress test site.
 
@@ -26,3 +26,35 @@ class TestTaxonomies(BaseTest):
         assert req['slug'] == self.wp_lib.getTaxonomyTag, "GetTaxonomyByTag endpoint didn't return correct slug."
         assert req['name'] == self.wp_lib.getTaxonomyName,\
             "Retrieved taxonomy from GetTaxonomyByTag endpoint does not have expected name."
+
+    def test_get_taxonomy_tag_that_does_not_exist(self):
+        """
+        Verify that the Get Taxonomy by Tag endpoint exhibits expected error behavior for a taxonomy tag that doesn't
+        exist
+        """
+        req = self.wp_tc.get_taxonomy_by_tag(self.wp_lib.getNonExistentTag)
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getTaxonomyNonExistentCode,
+                                                        self.wp_lib.getTaxonomyNonExistentMessage)
+
+    def test_get_taxonomy_tag_bad_tag(self):
+        """
+        Verify that the Get Taxonomy by Tag endpoint throws expected error for an invalid taxonomy tag
+        """
+        req = self.wp_tc.get_taxonomy_by_tag(self.wp_lib.getInvalidTag)
+        self.wp_lib.verify_response_item_is_invalid(req, self.wp_lib.getInvalidCode, self.wp_lib.getInvalidMessage)
+
+    def test_get_taxonomy_tag_max_size(self):
+        """
+        Verify that the Get Taxonomy by Tag endpoint throws expected error for a taxonomy tag using sys.maxsize
+        """
+        req = self.wp_tc.get_taxonomy_by_tag(str(self.maxsize))
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getTaxonomyNonExistentCode,
+                                                        self.wp_lib.getTaxonomyNonExistentMessage)
+
+    def test_get_taxonomy_tag_min_size(self):
+        """
+        Verify that the Get Taxonomy by Tag endpoint throws expected error for a taxonomy tag using -sys.maxsize
+        """
+        req = self.wp_tc.get_taxonomy_by_tag(str(self.minsize))
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getTaxonomyNonExistentCode,
+                                                        self.wp_lib.getTaxonomyNonExistentMessage)
