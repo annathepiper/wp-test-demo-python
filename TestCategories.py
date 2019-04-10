35aaf9579c3e91092d648d77c02780805ccb8b8f
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-10 09:55:33 -0700

    New negative test cases
    Adding new negative test cases as ported over from the Java suite.

--- TestCategories.py
+++ TestCategories.py
@@ -2,7 +2,7 @@ from BaseTest import BaseTest
 
 # TestCategories
 # Written by Angela Korra'ti
-# Last updated 2/18/2019
+# Last updated 4/10/2019
 #
 # This test class is for verifying the categories-related endpoints for the WordPress test site.
 
@@ -26,3 +26,34 @@ class TestCategories(BaseTest):
         assert req['id'] == self.wp_lib.getCategoryId, "GetCategoryById endpoint didn't return correct ID number."
         assert req['name'] == self.wp_lib.getCategoryName,\
             "Retrieved category from GetCategoryById endpoint does not have expected name."
+
+    def test_get_category_id_that_does_not_exist(self):
+        """
+        Verify that the Get Category by Id endpoint exhibits expected error behavior for a category ID that doesn't
+        exist
+        """
+        req = self.wp_tc.get_category_by_id(self.wp_lib.getNonExistentId)
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getTermNonExistentCode,
+                                                        self.wp_lib.getTermNonExistentMessage)
+
+    def test_get_category_id_bad_id(self):
+        """
+        Verify that the Get Category by Id endpoint throws expected error for an invalid category ID
+        """
+        req = self.wp_tc.get_category_by_id(self.wp_lib.getInvalidId)
+        self.wp_lib.verify_response_item_is_invalid(req, self.wp_lib.getInvalidCode, self.wp_lib.getInvalidMessage)
+
+    def test_get_category_id_max_size(self):
+        """
+        Verify that the Get Category by Id endpoint throws expected error for a category ID using sys.maxsize
+        """
+        req = self.wp_tc.get_category_by_id(str(self.maxsize))
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getTermNonExistentCode,
+                                                        self.wp_lib.getTermNonExistentMessage)
+
+    def test_get_category_id_min_size(self):
+        """
+        Verify that the Get Category by Id endpoint throws expected error for a category ID using -sys.maxsize
+        """
+        req = self.wp_tc.get_category_by_id(str(self.minsize))
+        self.wp_lib.verify_response_item_is_invalid(req, self.wp_lib.getInvalidCode, self.wp_lib.getInvalidMessage)
