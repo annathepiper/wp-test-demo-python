725a23ac3c9f5b4bc06ca0f2ebbd39e9a95da491
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-10 09:50:58 -0700

    New negative test cases
    Adding new negative test cases as ported over from the Java suite.

--- TestPages.py
+++ TestPages.py
@@ -2,7 +2,7 @@ from BaseTest import BaseTest
 
 # TestPages
 # Written by Angela Korra'ti
-# Last updated 2/18/2019
+# Last updated 4/10/2019
 #
 # This test class is for verifying the pages-related endpoints for the WordPress test site.
 
@@ -26,3 +26,33 @@ class TestPages(BaseTest):
         assert req['id'] == self.wp_lib.getPageId, "GetPageById endpoint didn't return correct ID number."
         assert req['title']['rendered'] == self.wp_lib.getPageTitle,\
             "Retrieved page from GetPageById endpoint does not have expected title."
+
+    def test_get_page_id_that_does_not_exist(self):
+        """
+        Verify that the Get Page by Id endpoint exhibits expected error behavior for a page ID that doesn't exist
+        """
+        req = self.wp_tc.get_page_by_id(self.wp_lib.getNonExistentId)
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getNonExistentCode,
+                                                        self.wp_lib.getNonExistentMessage)
+
+    def test_get_page_id_bad_id(self):
+        """
+        Verify that the Get Page by Id endpoint throws expected error for an invalid page ID
+        """
+        req = self.wp_tc.get_page_by_id(self.wp_lib.getInvalidId)
+        self.wp_lib.verify_response_item_is_invalid(req, self.wp_lib.getInvalidCode, self.wp_lib.getInvalidMessage)
+
+    def test_get_page_id_max_size(self):
+        """
+        Verify that the Get Page by Id endpoint throws expected error for a page ID using sys.maxsize
+        """
+        req = self.wp_tc.get_page_by_id(str(self.maxsize))
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getNonExistentCode,
+                                                        self.wp_lib.getNonExistentMessage)
+
+    def test_get_page_id_min_size(self):
+        """
+        Verify that the Get Page by Id endpoint throws expected error for a page ID using -sys.maxsize
+        """
+        req = self.wp_tc.get_page_by_id(str(self.minsize))
+        self.wp_lib.verify_response_item_is_invalid(req, self.wp_lib.getInvalidCode, self.wp_lib.getInvalidMessage)
