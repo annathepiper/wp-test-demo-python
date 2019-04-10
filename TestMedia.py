6e8170b8c06d8e56f27defab06aee053a8253739
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-10 10:15:29 -0700

    New negative test cases
    Adding new negative test cases as ported over from the Java suite.

--- TestMedia.py
+++ TestMedia.py
@@ -2,7 +2,7 @@ from BaseTest import BaseTest
 
 # TestMedia
 # Written by Angela Korra'ti
-# Last updated 3/6/2019
+# Last updated 4/10/2019
 #
 # This test class is for verifying the media-related endpoints for the WordPress test site.
 
@@ -31,3 +31,33 @@ class TestMedia(BaseTest):
         assert req['id'] == targetId, "GetMediaById endpoint didn't return correct ID number."
         assert req['title']['rendered'] == self.wp_lib.getMediaTitle,\
             "Retrieved media from GetMediaById endpoint does not have expected title."
+
+    def test_get_media_id_that_does_not_exist(self):
+        """
+        Verify that the Get Media by Id endpoint exhibits expected error behavior for a media ID that doesn't exist
+        """
+        req = self.wp_tc.get_media_by_id(self.wp_lib.getNonExistentId)
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getNonExistentCode,
+                                                        self.wp_lib.getNonExistentMessage)
+
+    def test_get_media_id_bad_id(self):
+        """
+        Verify that the Get Media by Id endpoint throws expected error for an invalid media ID
+        """
+        req = self.wp_tc.get_media_by_id(self.wp_lib.getInvalidId)
+        self.wp_lib.verify_response_item_is_invalid(req, self.wp_lib.getInvalidCode, self.wp_lib.getInvalidMessage)
+
+    def test_get_media_id_max_size(self):
+        """
+        Verify that the Get Media by Id endpoint throws expected error for a media ID using sys.maxsize
+        """
+        req = self.wp_tc.get_media_by_id(str(self.maxsize))
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getNonExistentCode,
+                                                        self.wp_lib.getNonExistentMessage)
+
+    def test_get_media_id_min_size(self):
+        """
+        Verify that the Get Media by Id endpoint throws expected error for a media ID using -sys.maxsize
+        """
+        req = self.wp_tc.get_media_by_id(str(self.minsize))
+        self.wp_lib.verify_response_item_is_invalid(req, self.wp_lib.getInvalidCode, self.wp_lib.getInvalidMessage)
