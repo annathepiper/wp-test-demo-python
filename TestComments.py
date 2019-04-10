41ce8805471838e3e84f26c5774974f38e875f73
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-10 10:02:59 -0700

    New negative test cases
    Adding new negative test cases as ported over from the Java suite.

--- TestComments.py
+++ TestComments.py
@@ -2,7 +2,7 @@ from BaseTest import BaseTest
 
 # TestComments
 # Written by Angela Korra'ti
-# Last updated 2/18/2019
+# Last updated 4/10/2019
 #
 # This test class is for verifying the comments-related endpoints for the WordPress test site.
 
@@ -26,3 +26,33 @@ class TestComments(BaseTest):
         assert req['id'] == self.wp_lib.getCommentId, "GetCommentById endpoint didn't return correct ID number."
         assert self.wp_lib.getCommentContent in req['content']['rendered'],\
             "Retrieved comment from GetCommentById endpoint does not have expected content."
+
+    def test_get_comment_id_that_does_not_exist(self):
+        """
+        Verify that the Get Comment by Id endpoint exhibits expected error behavior for a comment ID that doesn't exist
+        """
+        req = self.wp_tc.get_comment_by_id(self.wp_lib.getNonExistentId)
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getCommentNonExistentCode,
+                                                        self.wp_lib.getCommentNonExistentMessage)
+
+    def test_get_comment_id_bad_id(self):
+        """
+        Verify that the Get Comment by Id endpoint throws expected error for an invalid comment ID
+        """
+        req = self.wp_tc.get_comment_by_id(self.wp_lib.getInvalidId)
+        self.wp_lib.verify_response_item_is_invalid(req, self.wp_lib.getInvalidCode, self.wp_lib.getInvalidMessage)
+
+    def test_get_comment_id_max_size(self):
+        """
+        Verify that the Get Comment by Id endpoint throws expected error for a comment ID using sys.maxsize
+        """
+        req = self.wp_tc.get_comment_by_id(str(self.maxsize))
+        self.wp_lib.verify_response_item_does_not_exist(req, self.wp_lib.getCommentNonExistentCode,
+                                                        self.wp_lib.getCommentNonExistentMessage)
+
+    def test_get_comment_id_min_size(self):
+        """
+        Verify that the Get Comment by Id endpoint throws expected error for a comment ID using -sys.maxsize
+        """
+        req = self.wp_tc.get_comment_by_id(str(self.minsize))
+        self.wp_lib.verify_response_item_is_invalid(req, self.wp_lib.getInvalidCode, self.wp_lib.getInvalidMessage)
