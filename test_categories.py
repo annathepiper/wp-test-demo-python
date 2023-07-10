5ff5aa82729293b3a09694676016daaff45ac997
Angela Korra'ti <annathepiper@fastmail.com>
2023-07-10 14:34:05 -0700

    Renamed almost all files to Pytest compatible format, also corrected a couple of IDs to fix failing tests

--- test_categories.py
+++ test_categories.py
@@ -2,7 +2,7 @@ from BaseTest import BaseTest
 
 # TestCategories
 # Written by Angela Korra'ti
-# Last updated 4/10/2019
+# Last updated 7/10/2023
 #
 # This test class is for verifying the categories-related endpoints for the WordPress test site.
 
